69fcfa4e1dbe512bfa0935f74ab197ae7ba9a767
Gabriel Zarpelon <zarpelon091098@gmail.com>
2019-10-22 00:01:26 -0300

    Project modules

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const project = require('./controllers/project');
+const projectEvaluation = require('./controllers/project_evaluation');
 const mongo = require('koa-mongo')
 const app = new Koa();
 app.use(mongo({
@@ -23,7 +24,10 @@ app.use(logger());
 
 router.post('/project', project.createProject)
       .get('/project/:id', project.getProjectById)
-      .delete('/project/:id', project.deleteProject);
+      .delete('/project/:id', project.deleteProject)
+      
+      .post('/project_evaluation/:id', projectEvaluation.createProject )
+      .put('/project_evaluation/:id', projectEvaluation.updateProject );
 
 app.use(router.routes());
 
---
+++ controllers/project_evaluation.js
@@ -0,0 +1,21 @@
+const mongo = require('koa-mongo')
+
+const projectEvaluation = {
+    createProject: (ctx) =>{
+        return ctx.db.collection('project').updateOne({"_id": mongo.ObjectID(ctx.params.id)} , { $push :  { 'project_evaluation' : ctx.request.body } })
+        .then((results)=>{
+            ctx.body = results;
+            ctx.status = 200;
+        })
+        .catch(err =>{ctx.body = 'error: ' + err; ctx.status = 500;})
+    },
+    updateProject: (ctx) =>{
+        return ctx.db.collection('project').updateOne({"_id": mongo.ObjectID(ctx.params.id)} , { $pull :  { 'project_evaluation' : ctx.request.body } })
+        .then((results)=>{
+            ctx.body = results;
+            ctx.status = 200;
+        })
+        .catch(err =>{ctx.body = 'error: ' + err; ctx.status = 500;})
+    }
+}
+module.exports = projectEvaluation;
